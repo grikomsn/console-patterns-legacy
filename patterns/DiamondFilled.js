a363fdef9ea84b27cb6b72890c9e5e1f29748c07
Nirajan Rajkarnikar <nirajan.rajkarnikar@keybridgewireless.com>
2019-10-05 10:31:08 -0400

    Update DiamondFilled.js

--- patterns/DiamondFilled.js
+++ patterns/DiamondFilled.js
@@ -2,8 +2,8 @@ function DiamondFilled(n) {
   let out = ''
   for (let i = 0; i < 2 * n - 1; i++) {
     
-    if ( i < n ) { k = i }
-    else { k = 2 * n - i - 2 }
+    if ( i < n ) { let k = i }
+    else { let k = 2 * n - i - 2 }
 
     for (let j = 0; j < 2 * n - 1; j++) {
       const clause = ( j >= n - k - 1  && j <= n + k - 1 )
