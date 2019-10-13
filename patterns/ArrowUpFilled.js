8a0ebcc17e4cb3fcf63017a5c5f187199b08d848
Nirajan Rajkarnikar <nirajan.rajkarnikar@keybridgewireless.com>
2019-10-12 20:27:10 -0400

    Create ArrowUpFilled.js

---
+++ patterns/ArrowUpFilled.js
@@ -0,0 +1,39 @@
+function ArrowUpFilled(n) {
+  let out = ''
+
+  for (let i = 0; i < 2*n-1; i++) {
+    if (i<n){k = i}
+    else {k = n-i}
+    
+    for (let j = 0; j < 2*n-1; j++) {
+      const verticalCenter = j===n-1
+      const clause = (j>=n-k-1  && j<=n+k-1) || verticalCenter
+      out += clause ? '* ' : '  '
+    }
+    out += '\n'
+  }
+  return out
+}
+
+ArrowUpFilled.title = 'Up Arrow - Filled'
+
+ArrowUpFilled.source = `
+function ArrowUpFilled(n) {
+  let out = ''
+
+  for (let i = 0; i < 2*n-1; i++) {
+    if (i<n){k = i}
+    else {k = n-i}
+    
+    for (let j = 0; j < 2*n-1; j++) {
+      const verticalCenter = j===n-1
+      const clause = (j>=n-k-1  && j<=n+k-1) || verticalCenter
+      out += clause ? '* ' : '  '
+    }
+    out += '\\n'
+  }
+  return out
+}`
+
+
+export default ArrowUpFilled
