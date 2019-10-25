f122299f742caf2258e5d0fe66a5ceffd1696ea4
nirajanrk <nirajanrk@gmail.com>
2019-10-25 04:30:24 -0400

    Update HourglassEmptyVertical.js

--- patterns/HourglassEmptyVertical.js
+++ patterns/HourglassEmptyVertical.js
@@ -3,9 +3,9 @@ function HourglassEmptyVertical(n) {
   
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
-      diag1 = i === j
-      diag2 = j === n - i - 1
-      bottom = i === n - 1
+      const diag1 = i === j
+      const diag2 = j === n - i - 1
+      const bottom = i === n - 1
       const clause = diag1 || diag2 || bottom || i === 0
       out += clause ? '* ' : '  '
     }
@@ -22,9 +22,9 @@ function HourglassEmptyVertical(n) {
   
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
-      diag1 = i === j
-      diag2 = j === n - i - 1
-      bottom = i === n - 1
+      const diag1 = i === j
+      const diag2 = j === n - i - 1
+      const bottom = i === n - 1
       const clause = diag1 || diag2 || bottom || i === 0
       out += clause ? '* ' : '  '
     }
