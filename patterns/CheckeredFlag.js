92f1073cdff4938bd6039d720555740706439633
nbacombi <57240252+nbacombi@users.noreply.github.com>
2019-10-31 20:44:38 -0400

    Create CheckeredFlag.js

---
+++ patterns/CheckeredFlag.js
@@ -0,0 +1,34 @@
+function CheckeredFlag(n,hspacing,vspacing) {
+  let out = ''
+  
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      const even = i % 2 == 0 && j % 2 == 0
+      const odd = i % 2 != 0 && j % 2 != 0
+      const clause = even || odd
+      out += clause ? '█ ' : '  '
+    }
+    out += '\n'
+  }
+  return out
+}
+
+CheckeredFlag.title = 'Checkered Flag'
+
+CheckeredFlag.source = `
+function CheckeredFlag(n,hspacing,vspacing) {
+  let out = ''
+  
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      const even = i % 2 == 0 && j % 2 == 0
+      const odd = i % 2 != 0 && j % 2 != 0
+      const clause = even || odd
+      out += clause ? '█ ' : '  '
+    }
+    out += '\\n'
+  }
+  return out
+}`
+
+export default CheckeredFlag
