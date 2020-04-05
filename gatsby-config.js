7e8a6f13b28e2d0e8177ae400fd9d24600502971
Griko Nibras <git@griko.id>
2020-04-06 04:55:57 +0700

    Update deps and remove unused deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,17 +3,13 @@ const siteConfig = require('./site-config')
 module.exports = {
   siteMetadata: { ...siteConfig },
   plugins: [
-    'gatsby-plugin-catch-links',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sharp',
-    'gatsby-plugin-sitemap',
-    'gatsby-plugin-webpack-size',
     'gatsby-transformer-json',
-    'gatsby-transformer-sharp',
     {
       resolve: 'gatsby-source-filesystem',
       options: { name: 'images', path: `${__dirname}/src/images` },
     },
-    'gatsby-plugin-offline',
+    'gatsby-plugin-remove-serviceworker',
   ],
 }
